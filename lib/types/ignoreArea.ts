aa83da8352d8a5ede4d3c2936084dca65b3b356c
Pavel Strunkin <pashidlos@gmail.com>
2020-11-03 09:56:03 +0200

    Add ignoreAreas into testRun payload #44
    https://github.com/Visual-Regression-Tracker/sdk-js/issues/44

---
+++ lib/types/ignoreArea.ts
@@ -0,0 +1,6 @@
+export interface IgnoreArea {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
--- lib/types/testRun.ts
+++ lib/types/testRun.ts
@@ -1,3 +1,5 @@
+import { IgnoreArea } from "./ignoreArea";
+
 export interface TestRun {
   name: string;
   imageBase64: string;
@@ -6,4 +8,5 @@ export interface TestRun {
   viewport?: string;
   device?: string;
   diffTollerancePercent?: number;
+  ignoreAreas?: IgnoreArea[];
 }
